0c9d33ef32617b3ec08837575b10986c1ac117c2
vasiliv <ggo24@yahoo.com>
2019-05-12 13:25:56 +0400

    Read and Write in Category

---
+++ Blog.Service/AddCategory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Domain;
+using Blog.Repository;
+
+namespace Blog.Service {
+    public class AddCategory {
+        public AddCategory() {
+        }
+        public AddCategory(int id, string name, string description, bool isDeleted) {
+            var myCategory = new Category();
+            myCategory.ID = id;
+            myCategory.Name = name;
+            myCategory.Description = description;
+            myCategory.IsDeleted = false;
+
+            using (BlogDBContext myBlogDBContext = new BlogDBContext()) {
+                myBlogDBContext.Categoties.Add(myCategory);
+                myBlogDBContext.SaveChanges();
+            }
+        }
+        public void ReadCategory() {
+            using (BlogDBContext myBlogDBContext = new BlogDBContext()) {
+                var readCategory = from Category in myBlogDBContext.Categoties
+                                   select Category;
+
+                foreach (var x in readCategory) {
+                    int id = x.ID;
+                    string name = x.Name;
+                    string description = x.Description;
+                }
+            }
+        }    
+    }
+}
--- Blog.WebSite/default.aspx.cs
+++ Blog.WebSite/default.aspx.cs
@@ -5,14 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Blog.Repository;
+using Blog.Service;
 
 namespace Blog.WebSite
 {
     public partial class _default : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            var blogcon = new BlogDBContext();
+        protected void Page_Load(object sender, EventArgs e){
+            //AddCategory category = new AddCategory(1, "SciFi", "bla bla bla", false);
+            AddCategory read = new AddCategory();
+            read.ReadCategory();
         }
     }
 }
